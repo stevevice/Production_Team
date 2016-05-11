62044f2eed9a26429e303dcc17b76ec493f2d10a
atouchetAIE <cidleaf@gmail.com>
2016-05-11 13:51:49 -0500

    Created and started Testing Combat
    Ran into some problems between nav agent and collisions.

---
+++ Assets/Scripts/UnitAttributes.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class UnitAttributes : MonoBehaviour {
+
+    float health;
+    public List<Weapons> weaponsList;
+
+    //To caculate speed
+    float preTime;
+    Vector3 preVector;
+    float force;
+
+	// Use this for initialization
+	void Start () {
+        preTime = 0;
+        preVector = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float timeInt = Time.time - preTime;
+        Vector3 dist = gameObject.transform.position - preVector;
+
+        force = dist.sqrMagnitude / timeInt;
+        preTime = Time.time;
+        preVector = gameObject.transform.position;
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Weapon"))
+        {
+            float dam = other.gameObject.GetComponent<Weapons>().damage;
+            Debug.Log(dam);
+            //other.gameObject.GetComponentInParent<>();
+            
+            //health -= dam + ;
+        }
+    }
+}
