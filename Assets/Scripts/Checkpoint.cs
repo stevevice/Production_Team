e47f119f6de232e393388ab9b296310dabb64629
stevevice <steve_alexanders@bellsouth.net>
2016-05-17 15:39:56 -0500

    CheckPoint Script Revision For AI Use

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,28 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Checkpoint : MonoBehaviour
 {
-    public Transform Point;     //Where the Empty gameobject is
     public float AreaHit;       //How close the Unit must hit
-    bool PointHit = false;
-    public GameObject LastRacer;
+    public List<GameObject> Racers; //list that will not be edited in this script but in the RaceManager
 
-    public GameObject CheckPosition(GameObject CurrentPoint)
+    public bool CheckPosition(GameObject CurrentPoint)
     {
-        if ((Point.position.x - AreaHit) <= CurrentPoint.transform.position.x && CurrentPoint.transform.position.x <= (Point.position.x + AreaHit))
+        if ((gameObject.transform.position.x - AreaHit) <= CurrentPoint.transform.position.x && CurrentPoint.transform.position.x <= (gameObject.transform.position.x + AreaHit))
         {
-            if ((Point.position.z - AreaHit) <= CurrentPoint.transform.position.z && CurrentPoint.transform.position.z <= (Point.position.z + AreaHit))
+            if ((gameObject.transform.position.z - AreaHit) <= CurrentPoint.transform.position.z && CurrentPoint.transform.position.z <= (gameObject.transform.position.z + AreaHit))
             {
-                PointHit = true;
-                return CurrentPoint;
+                return true;
             }
         }
-        return null;
+        return false;
     }
 
-    public bool CheckPointHit()
+    void Start()
     {
-        return PointHit;
+        Racers = new List<GameObject>();
     }
 }
