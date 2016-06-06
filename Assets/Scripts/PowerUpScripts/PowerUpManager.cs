53b10f398790e82112bb84a548718f939bd0ad66
stevevice <steve_alexanders@bellsouth.net>
2016-06-06 17:58:35 -0500

    Power Up Manager (In Progress)

--- Assets/Scripts/PowerUpScripts/PowerUpManager.cs
+++ Assets/Scripts/PowerUpScripts/PowerUpManager.cs
@@ -1,15 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class PowerUpManager : MonoBehaviour {
+public class PowerUpManager : MonoBehaviour
+{
+    public List<GameObject> PowerUpList = new List<GameObject>();
 
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        GameObject[] SpeedPU = GameObject.FindGameObjectsWithTag("SpeedBoostPU");
+        GameObject[] HealthPU = GameObject.FindGameObjectsWithTag("HealthBoostPU");
+
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 }
