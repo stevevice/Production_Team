60acea10c38c10af5fbdccee5d6394b7d9ba0469
atouchetAIE <cidleaf@gmail.com>
2016-06-08 15:03:32 -0500

    Particles

---
+++ Assets/Scripts/DestroyAfter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyAfter : MonoBehaviour {
+
+    public float timeAlive;
+    float aliveTill;
+
+	// Use this for initialization
+	void Start () {
+        aliveTill = Time.time + timeAlive;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time >= aliveTill)
+            Destroy(gameObject);
+	}
+}
